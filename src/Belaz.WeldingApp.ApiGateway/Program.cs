314e1c0c7a3f2294872f713e353ed080c226fa67
Vladislav <vsoilol1701@gmail.com>
2023-01-02 16:19:57 +0300

    some fixed for api gateway

--- src/Belaz.WeldingApp.ApiGateway/Program.cs
+++ src/Belaz.WeldingApp.ApiGateway/Program.cs
@@ -1,32 +1,54 @@
 using Belaz.WeldingApp.ApiGateway.Config;
+using Microsoft.OpenApi.Models;
+using MMLib.Ocelot.Provider.AppConfiguration;
 using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",
+            optional: true, reloadOnChange: true)
+        .AddOcelotWithSwaggerSupport((o) =>
+        {
+            o.Folder = "Configuration";
+        })
+        .AddEnvironmentVariables();
+});*/
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json",
+        optional: true, reloadOnChange: true)
+    .AddOcelotWithSwaggerSupport((o) => { o.Folder = "Configuration"; })
     .AddEnvironmentVariables();
 
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot().AddAppConfiguration();;
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-var app = builder.Build();
-
-app.UsePathBase("/api");
-
-app.UseSwaggerForOcelotUI(options =>
+builder.Services.AddSwaggerGen(c =>
 {
-    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
-    options.DownstreamSwaggerEndPointBasePath = "/api/swagger/docs";
-    options.PathToSwaggerGenerator = "/swagger/docs";
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Welding API Gateway", Version = "v1" });
 });
 
-await app.UseOcelot();
+var app = builder.Build();
+
+app.UseSwagger();
+
+await app.UseSwaggerForOcelotUI(options =>
+{
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+    options.DownstreamSwaggerEndPointBasePath = "/swagger/docs";
+    options.PathToSwaggerGenerator = "/swagger/docs";
+}).UseOcelot();
 
 app.Run();
