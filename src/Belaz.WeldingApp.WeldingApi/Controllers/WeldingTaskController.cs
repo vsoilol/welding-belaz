db20c60d23fb5e1f7e6ad70f57290b7ccf924cea
Vladislav <vsoilol1701@gmail.com>
2023-01-31 22:33:02 +0300

    add bad request for GetAllRegistrarInfoAsync

--- src/Belaz.WeldingApp.WeldingApi/Controllers/WeldingTaskController.cs
+++ src/Belaz.WeldingApp.WeldingApi/Controllers/WeldingTaskController.cs
@@ -1,6 +1,8 @@
-﻿using Belaz.WeldingApp.WeldingApi.BusinessLayer.Services.Interfaces;
+﻿using System.Net;
+using Belaz.WeldingApp.WeldingApi.BusinessLayer.Services.Interfaces;
 using Belaz.WeldingApp.WeldingApi.Domain.Dtos.WeldingTask;
 using Microsoft.AspNetCore.Mvc;
+using BadRequestResult = WeldingApp.Common.Models.BadRequestResult;
 
 namespace Belaz.WeldingApp.WeldingApi.Controllers;
 
@@ -24,8 +26,16 @@ public class WeldingTaskController : ControllerBase
 
     [HttpGet("registrarInfo")]
     [ProducesResponseType(typeof(IEnumerable<WeldingTaskRegistrarInfoDto>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<IEnumerable<WeldingTaskRegistrarInfoDto>>> GetAllRegistrarInfoAsync()
+    public Task<ActionResult<IEnumerable<WeldingTaskRegistrarInfoDto>>> GetAllRegistrarInfoAsync()
     {
-        return await _weldingTaskService.GetAllRegistrarInfoAsync();
+        var badRequestResult = new BadRequestResult
+        {
+            Errors = "Method not implement",
+            StatusCode = (int)HttpStatusCode.NotImplemented,
+            Title = "Not implement Error"
+        };
+
+        return Task
+            .FromResult<ActionResult<IEnumerable<WeldingTaskRegistrarInfoDto>>>(BadRequest(badRequestResult));
     }
 }
