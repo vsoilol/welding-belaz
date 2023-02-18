6a2b3cadb3340a69e5c6101c101e85a3dfd4513d
Vladislav <vsoilol1701@gmail.com>
2023-02-18 17:03:09 +0300

    add WeldingEquipments to chief and master dtos

--- src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/ChiefDto.cs
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/ChiefDto.cs
@@ -4,42 +4,41 @@ using Belaz.WeldingApp.WeldingApi.Domain.Dtos.WeldingEquipment;
 using Belaz.WeldingApp.WeldingApi.Domain.Entities.Users;
 using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
 
-namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos
+namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos;
+
+public class ChiefDto : IMapFrom<Chief>
 {
-    public class ChiefDto : IMapFrom<Chief>
+    public Guid Id { get; set; }
+
+    public string RfidTag { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public ProductionAreaBriefDto ProductionArea { get; set; } = null!;
+
+    public IReadOnlyList<WeldingEquipmentBriefDto>? WeldingEquipments { get; set; }
+
+    public void Mapping(Profile profile)
     {
-        public Guid Id { get; set; }
-
-        public string RfidTag { get; set; } = null!;
-
-        public string FirstName { get; set; } = null!;
-
-        public string LastName { get; set; } = null!;
-
-        public string MiddleName { get; set; } = null!;
-
-        public ProductionAreaBriefDto ProductionArea { get; set; } = null!;
-
-        public WeldingEquipmentBriefDto? WeldingEquipment { get; set; }
-
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<Chief, ChiefDto>()
-                .ForMember(dto => dto.RfidTag,
-                    opt => opt
-                        .MapFrom(x => x.UserInfo.RfidTag))
-                .ForMember(dto => dto.FirstName,
-                    opt => opt
-                        .MapFrom(x => x.UserInfo.FirstName))
-                .ForMember(dto => dto.MiddleName,
-                    opt => opt
-                        .MapFrom(x => x.UserInfo.MiddleName))
-                .ForMember(dto => dto.LastName,
-                    opt => opt
-                        .MapFrom(x => x.UserInfo.LastName))
-                .ForMember(dto => dto.ProductionArea,
-                    opt => opt
-                        .MapFrom(x => x.UserInfo.ProductionArea));
-        }
+        profile.CreateMap<Chief, ChiefDto>()
+            .ForMember(dto => dto.RfidTag,
+                opt => opt
+                    .MapFrom(x => x.UserInfo.RfidTag))
+            .ForMember(dto => dto.FirstName,
+                opt => opt
+                    .MapFrom(x => x.UserInfo.FirstName))
+            .ForMember(dto => dto.MiddleName,
+                opt => opt
+                    .MapFrom(x => x.UserInfo.MiddleName))
+            .ForMember(dto => dto.LastName,
+                opt => opt
+                    .MapFrom(x => x.UserInfo.LastName))
+            .ForMember(dto => dto.ProductionArea,
+                opt => opt
+                    .MapFrom(x => x.UserInfo.ProductionArea));
     }
 }
