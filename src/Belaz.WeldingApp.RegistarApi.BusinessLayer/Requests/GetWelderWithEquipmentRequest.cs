d805aca1c024b15eb75baac7df00171b27046602
Vladislav <vsoilol1701@gmail.com>
2023-02-22 16:53:48 +0300

    Revert "some changes"
    This reverts commit 364056a272887947b4d8ca386c165fdf759afaec.
    
    # Conflicts:
    #	src/Belaz.WeldingApp.RegistarApi.BusinessLayer/Services/Implementations/RegistarService.cs
    #	src/Belaz.WeldingApp.RegistarApi.BusinessLayer/Services/Interfaces/IRegistarService.cs

--- src/Belaz.WeldingApp.RegistarApi.BusinessLayer/Requests/GetWelderWithEquipmentRequest.cs
+++ src/Belaz.WeldingApp.RegistarApi.BusinessLayer/Requests/GetWelderWithEquipmentRequest.cs
@@ -1,28 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
 
 namespace Belaz.WeldingApp.RegistarApi.BusinessLayer.Requests;
 
 public class GetWelderWithEquipmentRequest
 {
-    [Display(Name = "RegID")]
-    [JsonProperty(PropertyName = "RegID")]
     [JsonPropertyName("RegID")]
     public string RegistarId { get; set; } = null!;
 
-    [Display(Name = "EplRF")]
-    [JsonProperty(PropertyName = "EplRF")]
     [JsonPropertyName("EplRF")]
     public string WelderRfidTag { get; set; } = null!;
-
+    
     [JsonPropertyName("EqpRF")]
-    [Display(Name = "EqpRF")]
-    [JsonProperty(PropertyName = "EqpRF")]
     public string EquipmentRfidTag { get; set; } = null!;
-
+    
     [JsonPropertyName("SDT")]
-    [Display(Name = "SDT")]
-    [JsonProperty(PropertyName = "SDT")]
     public DateTime StartDateTime { get; set; }
 }
