a377a48f369ecfc99766b2b42d0a1c35ed1eb8d9
Vladislav <vsoilol1701@gmail.com>
2023-02-26 22:09:25 +0300

    add Workshop to WorkplaceDto

--- src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Workplace/WorkplaceDto.cs
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Workplace/WorkplaceDto.cs
@@ -1,5 +1,7 @@
-﻿using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Post;
+﻿using AutoMapper;
+using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Post;
 using Belaz.WeldingApp.WeldingApi.Domain.Dtos.ProductionArea;
+using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Workshop;
 using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
 
 namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.Workplace;
@@ -7,12 +9,34 @@ namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.Workplace;
 public class WorkplaceDto : IMapFrom<Entities.Production.Workplace>
 {
     public Guid Id { get; set; }
-    
+
     public string? IdFromSystem { get; set; }
-    
+
     public int Number { get; set; }
-        
+
     public PostBriefDto? Post { get; set; }
 
-    public ProductionAreaBriefDto? ProductionArea { get; set; }
+    public ProductionAreaBriefDto ProductionArea { get; set; } = null!;
+
+    public WorkshopBriefDto Workshop { get; set; } = null!;
+
+    public void Mapping(Profile profile)
+    {
+        profile
+            .CreateMap<Entities.Production.Workplace, WorkplaceDto>()
+            .ForMember(
+                dto => dto.ProductionArea,
+                opt => opt.MapFrom(x => x.Post == null ? x.ProductionArea : x.Post.ProductionArea)
+            )
+            .ForMember(
+                dto => dto.Workshop,
+                opt =>
+                    opt.MapFrom(
+                        x =>
+                            x.Post == null
+                                ? x.ProductionArea!.Workshop
+                                : x.Post.ProductionArea.Workshop
+                    )
+            );
+    }
 }
