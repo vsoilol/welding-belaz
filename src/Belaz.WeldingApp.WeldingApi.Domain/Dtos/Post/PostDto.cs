0bb813c17e5ff9a673fb27b81fcbe3e7cb1ccf91
Vladislav <vsoilol1701@gmail.com>
2023-02-26 22:05:25 +0300

    add workshop to postDto

--- src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Post/PostDto.cs
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Post/PostDto.cs
@@ -1,4 +1,6 @@
-﻿using Belaz.WeldingApp.WeldingApi.Domain.Dtos.ProductionArea;
+﻿using AutoMapper;
+using Belaz.WeldingApp.WeldingApi.Domain.Dtos.ProductionArea;
+using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Workshop;
 using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
 
 namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.Post;
@@ -6,12 +8,21 @@ namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.Post;
 public class PostDto : IMapFrom<Entities.Production.Post>
 {
     public Guid Id { get; set; }
-    
+
     public string? IdFromSystem { get; set; }
-    
+
     public int Number { get; set; }
 
     public string Name { get; set; } = null!;
 
     public ProductionAreaBriefDto ProductionArea { get; set; } = null!;
+
+    public WorkshopBriefDto Workshop { get; set; } = null!;
+
+    public void Mapping(Profile profile)
+    {
+        profile
+            .CreateMap<Entities.Production.Post, PostDto>()
+            .ForMember(dto => dto.Workshop, opt => opt.MapFrom(x => x.ProductionArea.Workshop));
+    }
 }
