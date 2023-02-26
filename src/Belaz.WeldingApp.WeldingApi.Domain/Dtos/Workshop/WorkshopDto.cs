03fa80543b6bee7807cdf33a594eb3165574f31f
Vladislav <vsoilol1701@gmail.com>
2023-02-26 22:02:21 +0300

    add list of production area to workshop

--- src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Workshop/WorkshopDto.cs
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/Workshop/WorkshopDto.cs
@@ -1,14 +1,17 @@
-﻿using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
+﻿using Belaz.WeldingApp.WeldingApi.Domain.Dtos.ProductionArea;
+using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
 
 namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.Workshop;
 
 public class WorkshopDto : IMapFrom<Entities.Production.Workshop>
 {
     public Guid Id { get; set; }
-    
+
     public string? IdFromSystem { get; set; }
 
     public string Name { get; set; } = null!;
 
     public int Number { get; set; }
+
+    public List<ProductionAreaBriefDto> ProductionAreas { get; set; } = null!;
 }
