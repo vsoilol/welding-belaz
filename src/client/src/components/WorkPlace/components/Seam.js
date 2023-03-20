65b1a22ce2f4a8e81e648480d3d462c54b152468
Vladislav <vsoilol1701@gmail.com>
2023-03-20 21:10:26 +0300

    update client

--- src/client/src/components/WorkPlace/components/Seam.js
+++ src/client/src/components/WorkPlace/components/Seam.js
@@ -89,7 +89,7 @@ export const Seam = ({
 
   };
 
-  
+
 
 
   const columns = {
@@ -244,7 +244,63 @@ export const Seam = ({
     const { children, value, indPanel } = props_panel;
     return <div hidden={value !== indPanel}>{children}</div>;
   };
+  ///Изменение заголовка модалки
+  function TitleTextModal(params) {
+    if (params === 7) {
+      return "Редактировать Сварочный шов"
+    }
 
+    if (params === 15) {
+      return "Добавить Сварочный шов"
+    }
+  }
+  //Запрос на редактирование или добавление
+  function SendData(variables) {
+
+
+    variables["workshopId"] = valueProdArea
+    variables["workshopNumber"] = SetValue(valueProdArea, 0)
+    variables["productionAreaId"] = valuetPosts
+    variables["productionAreaNumber"] = SetValue(valuetPosts, 1)
+
+    variables["technologicalProcessId"] = valuetTechProc
+
+    variables["seams"] = [valuetSeam]
+
+    variables["postId"] = valuetWorkPlace
+    variables["workplaceId"] = valueWorkplace
+
+
+    console.log("variables")
+    //Добавить Сварочный шов
+    if (isModalNumb == 15) {
+      addSeam(variables)
+    }
+    //Редактировать Сварочный шов
+    if (isModalNumb == 7) {
+      editSeam(variables)
+    }
+
+  }
+  function SetValue(valueId, index) {
+
+    ///area
+    if (index === 1) {
+      for (let index = 0; index < area.length; index++) {
+        if (area[index].id === valueId) {
+          return area[index].number
+        }
+      }
+    }
+    ///post
+    if (index === 2) {
+      for (let index = 0; index < posts.length; index++) {
+        if (posts[index].id === valueId) {
+          return posts[index].number
+        }
+      }
+    }
+  }
   ////////////////////////////////////////////////////////////////////
   return (
     <div className={styles.innerWrapper}>
@@ -272,7 +328,7 @@ export const Seam = ({
                       setValuetTechProc("")
                       setValuetPosts("")
                       setValuetWorkPlace("")
-                      console.log("Добавить шов")
+                      console.log("Добавить шоваааа")
 
                     },
                   },
@@ -292,7 +348,7 @@ export const Seam = ({
                       setvaluetKnots(rowData?.product?.mainProduct?.id)
                       setvaluetDetail(rowData?.product?.id)
 
-                      console.log("Редактировать шва")
+                      console.log("Редактировать шваааа")
                     },
                   },
                 ]
@@ -319,7 +375,7 @@ export const Seam = ({
 
 
 
-        <SeamForm 
+        {/* <SeamForm 
           valueProdArea={valueProdArea} 
           valuetPosts={valuetPosts}
           valuetTechProc={valuetTechProc}
@@ -353,9 +409,135 @@ export const Seam = ({
 
           valuetDetail={valuetDetail}
   
-        />
+        /> */}
+
+        <ModalWindow
+          isOpen={isModalOpen}
+          headerText={TitleTextModal(isModalNumb)}
+          setIsOpen={(state) => {
+            setIsModalOpen(state);
+            setModalData(null);
+          }}
+          wrapperStyles={{ width: 420 }}
+        >
+          <Formik
+            initialValues={initialValues}
+            enableReinitialize
+            onSubmit={(variables) => {
+              const { id, ...dataToSend } = variables;
+              setIsModalOpen(false);
+              SendData(variables)
+            }}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              values,
+              setFieldValue,
+              handleBlur,
+            }) => (
+              <form onSubmit={handleSubmit}>
+
+                <div className={styles.row}>
+                  <Input
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                    style={{ width: 380, height: 40, padding: "0 20px 0 30px" }}
+                    value={values.number}
+                    name="number"
+                    placeholder="Номер"
+                    onBlur={handleBlur}
+                    autocomplete="off"
+                  />
+                </div>
+
+                <div className={styles.row}>
+                  <Select
+                    name="valueWorkplace"
+                    value={valueWorkplace}
+                    width="380px"
+                    placeholder="Рабочее место"
+                    onChange={(event) => {
+                      setValueWorkplace(event.value)
+                    }}
+                    options={workplaceIdOptions}
+                  />
+                </div>
+
+                <div className={styles.row}>
+                  <Select
+                    name="valuetPosts"
+                    width="380px"
+                    value={valuetPosts}
+                    placeholder="Производственные участки"
+                    onChange={(event) => setValuetPosts(event.value)}
+                    options={optPosts}
+                  />
+                </div>
 
 
+                <div className={styles.row}>
+                  <Select
+                    name="valuetTechProc"
+                    width="380px"
+                    value={valuetTechProc}
+                    placeholder="Технологический процесс"
+                    onChange={(event) => setValuetTechProc(event.value)}
+                    options={TechProc}
+                  />
+                </div>
+
+
+                {/*изделие, узел и деталь */}
+                <div className={styles.row}>
+                  <Select
+                    name="valuetProduct"
+                    width="380px"
+                    value={valuetProduct}
+                    placeholder="Изделие"
+                    onChange={(event) => setvaluetProduct(event.value)}
+                    options={productsOptions}
+                  />
+                </div>
+                <div className={styles.row}>
+                  <Select
+                    name="valuetKnots"
+                    width="380px"
+                    value={valuetKnots}
+                    placeholder="Узел"
+                    onChange={(event) => setvaluetKnots(event.value)}
+                    options={knotsOptions}
+                  />
+                </div>
+                <div className={styles.row}>
+                  <Select
+                    name="valuetDetail"
+                    width="380px"
+                    value={valuetDetail}
+                    placeholder="Деталь"
+                    onChange={(event) => setvaluetDetail(event.value)}
+                    options={detailOptions}
+                  />
+                </div>
+
+
+
+
+
+                <div className={styles.row}>
+                  <Button
+                    type="submit"
+                  >
+                    {modalData ? "Сохранить" : "Создать"}
+                  </Button>
+                </div>
+              </form>
+
+            )}
+          </Formik>
+
+        </ModalWindow>
 
 
       </div>
