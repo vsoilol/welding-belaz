4bccd026bd4ec7be57c5cd1bdc7bbc06b027739a
Vladislav <vsoilol1701@gmail.com>
2023-03-23 12:03:28 +0300

    update client

--- src/client/src/components/Tasks/index.js
+++ src/client/src/components/Tasks/index.js
@@ -20,7 +20,7 @@ import Tab from "@material-ui/core/Tab";
 import { array } from "yup";
 
 
-
+import deleteIcon from "assets/icons/delete.png";
 
 const {
   Creators: { setError },
@@ -106,6 +106,7 @@ export const Tasks = ({
     masterId: modalData?.master?.masterId ?? "",
     instructionId: modalData?.instruction.id ?? "",
     id: modalData?.id ?? "",
+    date: modalData?.weldingDate ?? "",
   };
 
 
@@ -193,13 +194,22 @@ export const Tasks = ({
 
   const columns = [
     {
-      title: "№ задания»", field: "number",
+      title:"Удаление",
+      render:(rowData)=>{
+         return <img className={styles.deleteIcon} src={deleteIcon} onClick={()=>{
+          setdeleteTaskModal(true);
+          setidPlan(rowData?.id)
+        }}></img>
+      }
     },
+    {
+      title: "№ задания»", field: "number",
+    }, 
     {
       title: "Номер шва", field: "seam.number",
     },
     {
-      title: "Количество швов", field: "amountFromPlan",
+      title: "Количество швов", field: "manufacturedAmount",
     },
     {
       title: "Дата ", field: "weldingDate",
@@ -225,7 +235,120 @@ export const Tasks = ({
       field: "seam.product.name",
       customFilterAndSearch: (term, rowData) => {
         return rowData?.seam?.product?.name?.toLowerCase().includes(term.toLowerCase()) ||
-               rowData?.seam?.product?.number?.toLowerCase().includes(term.toLowerCase())
+          rowData?.seam?.product?.number?.toLowerCase().includes(term.toLowerCase())
+      },
+      render: (rowData) => {
+        if (rowData?.seam?.product) {
+          return <p>{rowData.seam.product.name} {rowData.seam.product.number}</p>
+        } else {
+          return <p>-</p>
+        }
+      }
+    },
+    {
+      title: "Наименование узла",
+      field: "seam.knot.name",
+      customFilterAndSearch: (term, rowData) => rowData?.seam?.knot?.name?.toLowerCase().includes(term.toLowerCase()),
+      render: (rowData) => rowData?.seam?.knot ?
+        <p>{`${rowData.seam.knot.name} ${rowData.seam.knot.number}`}</p> :
+        <p>-</p>
+    },
+    {
+      title: "Наименование детали",
+      field: "seam.detail.name",
+      customFilterAndSearch: (term, rowData) => rowData?.seam?.detail?.name?.toLowerCase().includes(term.toLowerCase()),
+      render: (rowData) => rowData?.seam?.detail ?
+        <p>{`${rowData.seam.detail.name} ${rowData.seam.detail.number}`}</p> :
+        <p>-</p>
+    },
+    {
+      title: "Оборудование  ( инвентарный номер )",
+      field: "weldingEquipment.factoryNumber",
+      customFilterAndSearch: (term, rowData) => rowData?.weldingEquipment?.factoryNumber?.toLowerCase().includes(term.toLowerCase()),
+      render: (rowData) => rowData?.weldingEquipment?.factoryNumber ?? "-"
+    },
+    {
+      title: "Исполнитель",
+      field: "welder",
+      customFilterAndSearch: (term, rowData) => {
+        const fullName = `${rowData?.welder?.middleName} ${rowData?.welder?.firstName} ${rowData?.welder?.lastName}`.toLowerCase();
+        return fullName.includes(term.toLowerCase());
+      },
+      render: (rowData) => rowData?.welder ?
+        <p>{`${rowData.welder.middleName} ${rowData.welder.firstName} ${rowData.welder.lastName}`}</p> :
+        <p>-</p>
+    },
+    {
+      title: "Руководитель сварочных работ",
+      field: "master",
+      customFilterAndSearch: (term, rowData) => {
+        const fullName = `${rowData?.master?.middleName} ${rowData?.master?.firstName} ${rowData?.master?.lastName}`.toLowerCase();
+        return fullName.includes(term.toLowerCase());
+      },
+      render: (rowData) => rowData?.master ?
+        <p>{`${rowData.master.middleName} ${rowData.master.firstName} ${rowData.master.lastName}`}</p> :
+        <p>-</p>
+    },
+    {
+      title: "Контролер",
+      field: "inspector",
+      customFilterAndSearch: (term, rowData) => {
+        const fullName = `${rowData?.inspector?.middleName} ${rowData?.inspector?.firstName} ${rowData?.inspector?.lastName}`.toLowerCase();
+        return fullName.includes(term.toLowerCase());
+      },
+      render: (rowData) => rowData?.inspector ?
+        <p>{`${rowData.inspector.middleName} ${rowData.inspector.firstName} ${rowData.inspector.lastName}`}</p> :
+        <p>-</p>
+    },
+    {
+      field: "url",
+      title: "Скачать паспорт",
+      render: (rowData) => (
+        <div
+          onClick={() => getDocument(rowData?.number)}
+          className={styles.downloadButton}
+        >
+          <SaveIcon />
+        </div>
+      ),
+      width: 54,
+    },
+  ];
+  const columnsWelder = [
+    {
+      title: "№ задания»", field: "number",
+    }, 
+    {
+      title: "Номер шва", field: "seam.number",
+    },
+    {
+      title: "Количество швов", field: "manufacturedAmount",
+    },
+    {
+      title: "Дата ", field: "weldingDate",
+    },
+    {
+      title: "Статус",
+      field: "status",
+      render: (rowData) => {
+        if (rowData?.status === 3) {
+          return <p className={styles.Done}>Завершено</p>;
+        } else {
+          return <p className={styles.InProcess}>В процессе</p>;
+        }
+      },
+      customFilterAndSearch: (term, rowData) => {
+        const statusString =
+          rowData?.status === 3 ? "Завершено" : "В процессе";
+        return statusString.toLowerCase().includes(term.toLowerCase());
+      },
+    },
+    {
+      title: "Наименование изделия",
+      field: "seam.product.name",
+      customFilterAndSearch: (term, rowData) => {
+        return rowData?.seam?.product?.name?.toLowerCase().includes(term.toLowerCase()) ||
+          rowData?.seam?.product?.number?.toLowerCase().includes(term.toLowerCase())
       },
       render: (rowData) => {
         if (rowData?.seam?.product) {
@@ -309,7 +432,6 @@ export const Tasks = ({
 
 
 
-
   const [value_panel, setValue] = useState(0);
   const ChangePanels = (event, newValue) => {
     setValue(newValue);
@@ -326,60 +448,80 @@ export const Tasks = ({
   const [AmountManufactured, setAmountManufactured] = useState(0);
   const [idPlan, setidPlan] = useState("");
   const [valChioseMaster, setvalChioseMaster] = useState(masters[0]?.id);
-  function ChangeManufacturedDefective(AmountManufactured) {
 
-    console.log({
+
+
+  function ChangeManufacturedDefective(AmountManufactured, variables) {
+
+
+    ////Редактирование даты задания 
+    api.put(`/WeldingTask/changeDate`, {
       "id": idPlan,
-      "amount": AmountManufactured
+      "date": new Date(variables.date).toLocaleDateString('ru-RU')
     })
+      .then((response) => { loadTasks() })
+      .catch((error) => { });
 
 
-    if (userRole === "Admin") {
-      ////поменять количество продукции (изделие, детали или узла) 
-      api.put(`/productAccount/amountFromPlan`, {
+    ////Редактирование количества швов 
+    api.put(`/WeldingTask/changeSeamAmount`, {
+      "id": idPlan,
+      "seamAmount": AmountManufactured
+    })
+      .then((response) => { loadTasks() })
+      .catch((error) => { });
+
+    ////Редактирование количества швов 
+    api.put(`/WeldingTask/changeSeamAmount`, {
+      "id": idPlan,
+      "seamAmount": AmountManufactured
+    })
+      .then((response) => { loadTasks() })
+      .catch((error) => { });
+
+
+
+    ////Редактирование статуса 
+    if (isChecked1===true) {
+      api.put(`/WeldingTask/changeStatus`, {
         "id": idPlan,
-        "amount": AmountManufactured
-      })
-        .then((response) => {
-          // пересоздать задания и количество швов
-          api.post(`/productAccount/generateTasks`, {
-            "date": new Date().toLocaleDateString('ru-RU'),
-            "productionAreaId": masters.find(obj => obj.id === valChioseMaster)?.productionArea.id,
-            "masterId": masters.find(obj => obj.id === valChioseMaster)?.id,
-          })
-            .then((response) => {
-              window.location.reload()
-            })
-            .catch((error) => { });
-        })
-        .catch((error) => { });
-
+        "status": 3
+       })
+         .then((response) => { loadTasks() })
+         .catch((error) => { });
     }
-    else {
-
-
-      ////поменять количество продукции (изделие, детали или узла) 
-      api.put(`/productAccount/amountFromPlan`, {
+    else{
+      api.put(`/WeldingTask/changeStatus`, {
         "id": idPlan,
-        "amount": AmountManufactured
-      })
-        .then((response) => {
-          // пересоздать задания и количество швов
-          api.post(`/productAccount/generateTasks`, {
-            "date": new Date().toLocaleDateString('ru-RU'),
-            "productionAreaId": localStorage.getItem('USER_productionAreaId'),
-            "masterId": masters[0].id
-          })
-            .then((response) => {
-              window.location.reload()
-            })
-            .catch((error) => { });
-        })
-        .catch((error) => { });
+        "status": 1
+       })
+         .then((response) => { loadTasks() })
+         .catch((error) => { });
+    } 
+  }
 
 
+  const [isChecked1, setIsChecked1] = useState(false);
+  const [isChecked2, setIsChecked2] = useState(false);
+  const [dateTask, setdateTask] = useState("");
+
+  const handleCheckboxChange = (checkboxIndex) => {
+    if (checkboxIndex === 1) {
+      setIsChecked1(true);
+      setIsChecked2(false);
+    } else if (checkboxIndex === 2) {
+      setIsChecked1(false);
+      setIsChecked2(true);
     }
+  };
 
+
+  const [deleteTaskModal, setdeleteTaskModal] = useState(false);
+
+  function deleteTaskAjax() {  
+    api.remove(`/WeldingTask/${idPlan}`)
+      .then((response) => { loadTasks() })
+      .catch((error) => { });
   }
   return (
     <div className={styles.innerWrapper}>
@@ -410,9 +552,33 @@ export const Tasks = ({
         >
           <Table
             title="Сменные задания на сварку"
-            columns={columns}
+            columns={userRole === "Admin" || userRole === "Master"?columns:columnsWelder}
             data={combinedArray}
-            isLoading={isRequesting} 
+            isLoading={isRequesting}
+            actions={
+              userRole === "Admin" || userRole === "Master"
+                ? [
+                  {
+                    icon: "edit",
+                    tooltip: "Редактировать ",
+                    onClick: (event, rowData) => {
+                      setAmountManufactured(rowData?.manufacturedAmount)
+                      setidPlan(rowData?.id)
+                      setmodalchangeInfoproductAccount(true) 
+                      if (rowData?.status===3) {
+                        setIsChecked1(true)
+                        setIsChecked2(false)
+                      }
+                      else{
+                        setIsChecked1(false)
+                        setIsChecked2(true)
+                      }   
+                      setdateTask(rowData?.weldingDate.split(".").reverse().join("-")) 
+                    },
+                  },
+                ]
+                : []
+            }
           />
         </TabPanel>
 
@@ -454,7 +620,7 @@ export const Tasks = ({
             onSubmit={(variables) => {
               const { id, ...dataToSend } = variables;
               setmodalchangeInfoproductAccount(false)
-              ChangeManufacturedDefective(AmountManufactured)
+              ChangeManufacturedDefective(AmountManufactured, variables)
             }}
           >
             {({
@@ -477,12 +643,52 @@ export const Tasks = ({
                       style={{ height: 40, padding: "0 20px 0 30px", width: "100%" }}
                       value={AmountManufactured}
                       name="AmountManufactured"
-                      placeholder="Количество забракованной продукции"
+                      placeholder="количества продукции из плана"
                       onBlur={handleBlur}
                       autocomplete="off"
                     />
-
                   </div>
+
+                  <p style={{ padding: "15px 20px 0 30px" }}>Изменение даты задания </p>
+                  <div className={styles.row}>
+
+                    <Input
+                      onChange={(e) => {
+                        handleChange(e);   
+                        setdateTask(e.target.value)
+                      }}
+                      width="200"
+                      style={{ height: 40, padding: "0 20px 0 30px", width: 380 }}
+                      value={dateTask}
+                      name="date"
+                      placeholder="Дата "
+                      type="text"
+                      onFocus={(e) => {
+                        e.currentTarget.type = "date";
+                      }}
+                      onBlur={handleBlur}
+                      autocomplete="off"
+                    />
+                  </div>
+
+                  <p style={{ padding: "15px 20px 0 30px" }}>Изменение статус задания </p>
+                  <div className={styles.rowCheck}>
+                    <div className={styles.row}  onClick={() => handleCheckboxChange(1)}>
+                      <input
+                        type="checkbox"
+                        checked={isChecked1} 
+                      />
+                      <span className={styles.Done}>Завершено</span>
+                    </div>
+                    <div className={styles.row} onClick={() => handleCheckboxChange(2)}>
+                      <input
+                        type="checkbox"
+                        checked={isChecked2} 
+                      />
+                      <span className={styles.InProcess}>В процессе</span>
+                    </div>
+                  </div>
+
 
                   <div className={styles.row}>
                     <Button
@@ -492,6 +698,51 @@ export const Tasks = ({
                       type="submit"
                     >
                       Изменить
+                    </Button>
+                  </div>
+
+                </div>
+              </form>
+            )}
+          </Formik>
+        </ModalWindow>
+        
+
+        {/*Удаление задания*/}
+        <ModalWindow
+          isOpen={deleteTaskModal}
+          headerText="Удаление"
+          setIsOpen={(state) => {
+            setdeleteTaskModal(false)
+          }}
+          wrapperStyles={{ width: 420 }}
+        >
+          <Formik
+            initialValues={initialValues}
+            enableReinitialize
+            onSubmit={(variables) => {
+              const { id, ...dataToSend } = variables;
+              setdeleteTaskModal(false) 
+              deleteTaskAjax()
+            }}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              values,
+              setFieldValue,
+              handleBlur,
+            }) => (
+              <form onSubmit={handleSubmit}>
+
+                <div>
+                  <h4 style={{ padding: "35px 40px" }}>Вы уверены что хотите <span>удалить</span> данное задание ? </h4> 
+
+                  <div className={styles.row}>
+                    <Button 
+                      type="submit"
+                    >
+                      Удалить
                     </Button>
                   </div>
 
