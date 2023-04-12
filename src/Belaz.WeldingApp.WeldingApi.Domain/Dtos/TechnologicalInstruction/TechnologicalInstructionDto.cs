f7f25a5ca9ac0ee67f2586ca22be822153741005
Vladislav <vsoilol1701@gmail.com>
2023-04-12 12:30:20 +0300

    order by number for TechnologicalInstruction

--- src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/TechnologicalInstruction/TechnologicalInstructionDto.cs
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Dtos/TechnologicalInstruction/TechnologicalInstructionDto.cs
@@ -1,4 +1,5 @@
-﻿using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Seam;
+﻿using AutoMapper;
+using Belaz.WeldingApp.WeldingApi.Domain.Dtos.Seam;
 using Belaz.WeldingApp.WeldingApi.Domain.Mappings;
 
 namespace Belaz.WeldingApp.WeldingApi.Domain.Dtos.TechnologicalInstruction;
@@ -15,4 +16,17 @@ public class TechnologicalInstructionDto
     public List<SeamDto> Seams { get; set; } = null!;
 
     public List<WeldPassageInstructionDto> WeldPassageInstructions { get; set; } = null!;
+
+    public void Mapping(Profile profile)
+    {
+        profile
+            .CreateMap<
+                Entities.TechnologicalProcessInfo.TechnologicalInstruction,
+                TechnologicalInstructionDto
+            >()
+            .ForMember(
+                dto => dto.WeldPassageInstructions,
+                opt => opt.MapFrom(x => x.WeldPassageInstructions.OrderBy(_ => _.Number))
+            );
+    }
 }
