429e57458ed2b4f767564eff15ca9afe4d2c24e2
Vladislav <vsoilol1701@gmail.com>
2023-04-19 20:44:40 +0300

    update client

---
+++ src/client/src/assets/pics/index.js
@@ -0,0 +1,10 @@
+export { default as executorsImage } from "./executors.jpg";
+export { default as equipmentImage } from "./equipment.jpg";
+
+export { default as detalsImage } from "./detals.jpg";
+export { default as workPlaceImage } from "./workPlace.jpg";
+export { default as TechProcWeldImage } from "./TechProcWeld.jpg";
+
+export { default as tasksImage } from "./tasks.jpg";
+export { default as instructionsImage } from "./instructions.jpg";
+export { default as reportsImage } from "./reports.jpg";
