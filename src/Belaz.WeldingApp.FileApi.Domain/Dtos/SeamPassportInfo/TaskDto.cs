682a5e8420e1443d3d4f7f6b93c02add87f35f23
Vladislav <vsoilol1701@gmail.com>
2023-04-21 21:39:55 +0300

    add order for weldPassages when generate seam passport

--- src/Belaz.WeldingApp.FileApi.Domain/Dtos/SeamPassportInfo/TaskDto.cs
+++ src/Belaz.WeldingApp.FileApi.Domain/Dtos/SeamPassportInfo/TaskDto.cs
@@ -71,6 +71,16 @@ public class TaskDto : IMapFrom<WeldingTask>
             .ForMember(
                 dto => dto.WeldingEquipment,
                 opt => opt.MapFrom(x => x.WeldPassages.First().WeldingRecord.WeldingEquipment)
+            )
+            .ForMember(
+                dto => dto.WeldPassages,
+                opt =>
+                    opt.MapFrom(
+                        x =>
+                            x.WeldPassages
+                                .OrderBy(_ => _.WeldingRecord.Date.Date)
+                                .ThenBy(_ => _.WeldingRecord.WeldingStartTime.TotalSeconds)
+                    )
             );
     }
 }
