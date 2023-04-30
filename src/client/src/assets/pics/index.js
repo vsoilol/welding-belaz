db361181fa285a84e81004812427055346932d75
Vladislav <vsoilol1701@gmail.com>
2023-04-30 17:52:46 +0300

    delete unusing data

--- src/client/src/assets/pics/index.js
+++
@@ -1,9 +0,0 @@
-export { default as executorsImage } from "./executors.jpg";
-export { default as equipmentImage } from "./equipment.jpg";
-
-export { default as detalsImage } from "./detals.jpg";
-export { default as workPlaceImage } from "./workPlace.jpg";
-export { default as TechProcWeldImage } from "./TechProcWeld.jpg";
-
-export { default as tasksImage } from "./tasks.jpg";
-export { default as instructionsImage } from "./instructions.jpg";
