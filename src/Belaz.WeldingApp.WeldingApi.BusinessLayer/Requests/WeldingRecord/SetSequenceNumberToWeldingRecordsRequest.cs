25140d8ecb492c1734edcefbe26a9f8dc0b90892
Vladislav <vsoilol1701@gmail.com>
2023-06-20 22:58:25 +0300

    add SequenceNumber to welding record

---
+++ src/Belaz.WeldingApp.WeldingApi.BusinessLayer/Requests/WeldingRecord/SetSequenceNumberToWeldingRecordsRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Belaz.WeldingApp.WeldingApi.BusinessLayer.Requests.WeldingRecord;
+
+public class SetSequenceNumberToWeldingRecordsRequest
+{
+    
+}
---
+++ src/Belaz.WeldingApp.WeldingApi.BusinessLayer/Validations/Features/WeldingRecord/SetSequenceNumberToWeldingRecordsRequestValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace Belaz.WeldingApp.WeldingApi.BusinessLayer.Validations.Features.WeldingRecord;
+
+public class SetSequenceNumberToWeldingRecordsRequestValidator
+{
+    
+}
