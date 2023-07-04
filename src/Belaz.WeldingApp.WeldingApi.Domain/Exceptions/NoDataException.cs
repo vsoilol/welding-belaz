c368e9cd967367ced2f4fdf48e52a0eae3c02061
Vladislav <vsoilol1701@gmail.com>
2023-07-04 13:53:11 +0300

    check if full records is empty

---
+++ src/Belaz.WeldingApp.WeldingApi.Domain/Exceptions/NoDataException.cs
@@ -0,0 +1,9 @@
+﻿namespace Belaz.WeldingApp.WeldingApi.Domain.Exceptions;
+
+public class NoDataException : Exception
+{
+    public NoDataException() : base("Data is empty")
+    {
+        
+    }
+}
