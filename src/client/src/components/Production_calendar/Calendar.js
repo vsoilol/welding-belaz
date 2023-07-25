dbe65a86aec2dd0d7a9249dd3c6db4716541df12
Vladislav <vsoilol1701@gmail.com>
2023-07-25 16:42:19 +0300

    update client

--- src/client/src/components/Production_calendar/Calendar.js
+++ src/client/src/components/Production_calendar/Calendar.js
@@ -11,7 +11,7 @@ import api from "services/api";
 
 const localizer = momentLocalizer(moment);
 
-const Calendars = ({ valueWorkDays, WorkingShiftOptions }) => {
+const Calendars = ({ valueWorkDays, WorkingShiftOptions , loadCalendarYear}) => {
   const [selectedEvent, setSelectedEvent] = useState(null);
 
 
@@ -24,7 +24,7 @@ const Calendars = ({ valueWorkDays, WorkingShiftOptions }) => {
   const handleSelectEvent = (event) => {
     setSelectedEvent(event);
   };
- 
+
 
   const now = new Date();
   const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
@@ -48,31 +48,38 @@ const Calendars = ({ valueWorkDays, WorkingShiftOptions }) => {
     setevents([]);
     let events = [];
     for (let i = new Date(now.getFullYear(), now.getMonth(), 1).getDate(); i <= new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate(); i++) {
-      const existingDay = valueWorkDays?.find(day => day.number === i && day.monthNumber === now.getMonth() + 1);
-     
+      const existingDay = valueWorkDays?.find(day => day.number === i && day.monthNumber === now.getMonth() + 1); 
       if (!existingDay) {
         const shift1 = WorkingShiftOptions.find(elem => elem.number === 1);
         const shift2 = WorkingShiftOptions.find(elem => elem.number === 2);
-        const shift3 = WorkingShiftOptions.find(elem => elem.number === 3);
-  
+        const shift3 = WorkingShiftOptions.find(elem => elem.number === 3); 
         events.push(
           {
-            id: `event-${i}-1`,
+            id: existingDay?.id,
             title: `Смена 1`,
             start: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift1?.shiftStart?.substring(0, 2)), parseInt(shift1?.shiftStart?.substring(3, 5))),
             end: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift1?.shiftEnd?.substring(0, 2)), parseInt(shift1?.shiftEnd?.substring(3, 5))),
           },
           {
-            id: `event-${i}-2`,
+            id: existingDay?.id,
             title: `Смена 2`,
             start: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift2?.shiftStart?.substring(0, 2)), parseInt(shift2?.shiftStart?.substring(3, 5))),
             end: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift2?.shiftEnd?.substring(0, 2)), parseInt(shift2?.shiftEnd?.substring(3, 5))),
           },
           {
-            id: `event-${i}-3`,
+            id: existingDay?.id,
             title: `Смена 3`,
             start: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift3?.shiftStart?.substring(0, 2)), parseInt(shift3?.shiftStart?.substring(3, 5))),
             end: new Date(now.getFullYear(), now.getMonth(), i, parseInt(shift3?.shiftEnd?.substring(0, 2)), parseInt(shift3?.shiftEnd?.substring(3, 5))),
+          }
+        );
+      } else {
+        events.push(
+          {
+            id: existingDay.id,  
+            title: `Выходной день`,
+            start: new Date(now.getFullYear(), now.getMonth(), i),
+            end: new Date(now.getFullYear(), now.getMonth(), i),
           }
         );
       }
@@ -80,7 +87,18 @@ const Calendars = ({ valueWorkDays, WorkingShiftOptions }) => {
     setevents(events);
   }
 
-
+  function setWorkDay(date) {  
+    const data = {
+      "id": date.id,
+      "monthNumber": new Date(date?.start).getMonth()+1,
+      "number": new Date(date.start).getDate(),
+      "isWorkingDay": true
+    }
+    api.remove(`day/${date.id}`).then(()=>{ 
+      setSelectedEvent(false)
+      loadCalendarYear()
+    }) 
+  }
 
   function handleMonthChange(date) {
     getDays(date)
@@ -101,18 +119,20 @@ const Calendars = ({ valueWorkDays, WorkingShiftOptions }) => {
         views={['month']}
         messages={messages}
       />
+
       {selectedEvent && (
         <div>
-          <div className={styles.hints} >
+          <div className={styles.hints}>
             <h2>{selectedEvent.title}</h2>
             <p>Начало: {selectedEvent.start.toLocaleString()}</p>
             <p>Конец: {selectedEvent.end.toLocaleString()}</p>
+            {selectedEvent.title === "Выходной день" && (
+              <button onClick={() => setWorkDay(selectedEvent)}>Сделать рабочим</button>
+            )}
           </div>
           <div className={styles.hintsBg} onClick={() => { setSelectedEvent(false); setvaluechange(false) }}></div>
         </div>
-
-      )
-      }
+      )}
     </div >
   );
 };
