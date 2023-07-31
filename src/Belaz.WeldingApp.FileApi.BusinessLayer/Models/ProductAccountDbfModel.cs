9de3548dff753ee49156d4ceec31bb50691443e6
Vladislav <vsoilol1701@gmail.com>
2023-07-31 14:02:42 +0300

    add upload product accounts from dbf file

---
+++ src/Belaz.WeldingApp.FileApi.BusinessLayer/Models/ProductAccountDbfModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Belaz.WeldingApp.FileApi.BusinessLayer.Models;
+
+public class ProductAccountDbfModel
+{
+    public string? ProductIndex { get; set; }
+
+    public string? DetailNumber { get; set; }
+
+    public string? Suffix { get; set; }
+
+    public DateTime Date { get; set; }
+    
+    public int AmountFromPlan { get; set; }
+}
--- src/Belaz.WeldingApp.FileApi.BusinessLayer/Services/Interfaces/IUploadFileService.cs
+++ src/Belaz.WeldingApp.FileApi.BusinessLayer/Services/Interfaces/IUploadFileService.cs
@@ -1,5 +1,4 @@
-﻿using Belaz.WeldingApp.FileApi.BusinessLayer.Models;
-using LanguageExt;
+﻿using LanguageExt;
 using LanguageExt.Common;
 using Microsoft.AspNetCore.Http;
 
@@ -8,6 +7,6 @@ namespace Belaz.WeldingApp.FileApi.BusinessLayer.Services.Interfaces;
 public interface IUploadFileService
 {
     Task<Result<Unit>> UploadProductAccountDataAsync(IFormFile formFile);
-    
+
     Task<Result<Unit>> UploadUsersDataAsync(IFormFile formFile);
 }
