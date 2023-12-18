67a35a8a74be00944da3de8897ce5eb7c06a092e
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-12-18 23:39:56 +0300

    feat: add migrations which change seam number and SequenceNumber type

---
+++ src/Belaz.WeldingApp.Migrations/_202312180003_ChangeWeldingRecordSequenceNumber.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Belaz.WeldingApp.Migrations;
+
+[Migration(202312180003)]
+public class _202312180003_ChangeWeldingRecordSequenceNumber : Migration
+{
+    public override void Up()
+    {
+        Alter.Table("WeldingRecords")
+            .InSchema("public")
+            .AlterColumn("SequenceNumber")
+            .AsString()
+            .Nullable();
+    }
+
+    public override void Down()
+    {
+        Execute.Sql(
+            "ALTER TABLE \"public\".\"WeldingRecords\" ALTER COLUMN \"SequenceNumber\" TYPE integer USING (\"SequenceNumber\"::integer);");
+    }
+}
