4353f44cf6a2f3b4a331b937f0d31f776ce60626
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-12-18 22:54:09 +0300

    feat: add fluent migrator and add this to docker compose

---
+++ src/Belaz.WeldingApp.MigrationUtil/App.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+
+namespace Belaz.WeldingApp.MigrationUtil;
+
+public class App
+{
+    private readonly IMigrationRunner _migrationRunner;
+    private readonly IConfiguration _configuration;
+
+    public App(IMigrationRunner migrationRunner, IConfiguration configuration)
+    {
+        _migrationRunner = migrationRunner;
+        _configuration = configuration;
+    }
+
+    public Task ExecuteAsync(CancellationToken stoppingToken = default)
+    {
+        var isDowngrade = bool.Parse(_configuration["isDowngrade"]!);
+        var version = long.Parse(_configuration["version"]!);
+        
+        _migrationRunner.ListMigrations();
+
+        if (isDowngrade)
+        {
+            _migrationRunner.MigrateDown(version != -1 ? version : 0);
+            return Task.CompletedTask;
+        }
+
+        if (version == -1)
+        {
+            _migrationRunner.MigrateUp();
+        }
+        else
+        {
+            _migrationRunner.MigrateUp(version);
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Belaz.WeldingApp.Migrations/_202312180001_InitialTables.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Belaz.WeldingApp.Migrations;
+
+[Migration(202312180001)]
+public class _202312180001_InitialTables : Migration
+{
+    public override void Up()
+    {
+        Execute.EmbeddedScript("Belaz.WeldingApp.Migrations.Scripts.CreateDatabaseSchema.sql");
+    }
+
+    public override void Down()
+    {
+        Execute.EmbeddedScript("Belaz.WeldingApp.Migrations.Scripts.DropDatabaseSchema.sql");
+    }
+}
