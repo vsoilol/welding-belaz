f024597c79e3c38d756e4eadc65f86d849b75946
Vladislav Sokolov <vsoilol1701@gmail.com>
2024-04-18 14:47:01 +0300

    fix: delete unusing scripts

--- terminal-scripts/windows/restore_database_docker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set filePath=../../database/init-script.sql
-set containerId=postgresql_database
-set database=welding-belaz
-set username=admin
-
-type %filePath% | docker exec -i %containerId% psql -U %username% -d %database%
