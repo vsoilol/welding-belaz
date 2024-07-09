f991d071a90e81eeb547b13a8153ab8d672c485f
Vladislav Sokolov <vsoilol1701@gmail.com>
2024-07-09 16:38:37 +0300

    fix: fix script run-all-project.ps1

--- terminal-scripts/powershell/run-all-projects.ps1
+++ terminal-scripts/powershell/run-all-projects.ps1
@@ -1,5 +1,6 @@
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project ../../src/Belaz.WeldingApp.WeldingApi"
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project ../../src/Belaz.WeldingApp.RegistarApi"
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project ../../src/Belaz.WeldingApp.FileApi"
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project ../../src/Belaz.WeldingApp.IdentityApi"
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "run --project ../../src/Belaz.WeldingApp.ApiGateway"
+Start-Process powershell -ArgumentList "cd ../../src/Belaz.WeldingApp.WeldingApi; dotnet run"
+Start-Process powershell -ArgumentList "cd ../../src/Belaz.WeldingApp.RegistarApi; dotnet run"
+Start-Process powershell -ArgumentList "cd ../../src/Belaz.WeldingApp.FileApi; dotnet run"
+Start-Process powershell -ArgumentList "cd ../../src/Belaz.WeldingApp.IdentityApi; dotnet run"
+Start-Process powershell -ArgumentList "cd ../../src/Belaz.WeldingApp.ApiGateway; dotnet run"
+# Add more projects as needed
